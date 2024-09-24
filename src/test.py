e68a493af8a7ed1d345b0cc9f63b75364abba4d3
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-09-24 23:22:03 +0100

    add test

---
+++ src/test.py
@@ -0,0 +1,27 @@
+# this is a comment
+
+# hash table
+graph = {}
+
+graph["You"] = ["Daniel"]
+
+print(graph["You"])
+
+# bool variable
+odd = bool(1)
+
+# if statement
+if odd == 1:
+    print("My name is Tiffany")
+
+# int variable
+num = int(4)
+
+if num % 2 == 0:
+    print("That is an even number!")
+
+# list
+shapes = ["Square", "Circle", "Triangle"]
+x, y, z = shapes
+
+print(x + " " + y + " " + z)
