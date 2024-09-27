9de3ad93525524a64a6f406da4f7c06193757892
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-09-27 17:46:44 +0100

    add stringsSlice and stringsModify

---
+++ src/stringsModify.py
@@ -0,0 +1,22 @@
+def main():
+    print("Hello World")
+
+if __name__ == '__main__':
+    main()
+
+x = ("Tiffany")
+
+print(x.upper())
+print(x.lower())
+
+y = (" Tiffypox")
+
+print(y.strip()) # removes whitespace from beginning or end
+
+print(y.replace("p", "b"))
+
+print(y.split("p"))
+
+z = ("gREEN fROG")
+
+print(z.swapcase())
