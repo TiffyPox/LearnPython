48f6fd0ccedac92090f523483a25d53ead2b3e11
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-09-29 19:43:55 +0100

    add whileLoop

---
+++ src/whileLoop.py
@@ -0,0 +1,40 @@
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
+
+i = int(1)
+
+while i < 11:
+    print(i)
+    i += 1
+
+# Using break to exit the loop:
+j = int(1)
+
+while j < 6:
+    print(j)
+    if j == 3:
+        break
+    j += 1
+
+# Using continue to stop the current iteration and continue:
+
+k = int(0)
+
+while k < 6:
+    k += 1
+    if k == 3:
+        continue # skips three
+    print(k)
+
+# Using the else statement:
+
+l = int(1)
+
+while l < 11:
+    print(l)
+    l += 1
+else:
+    print("Variable is no longer less than 11")
