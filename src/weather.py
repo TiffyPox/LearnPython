14f3242180eb38057cc986ea651e4c952849e4e2
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2024-10-25 16:52:57 +0100

    update weather

--- src/weather.py
+++ src/weather.py
@@ -1,6 +1,7 @@
 from msvcrt import getch
 #import datetime as dt
 import requests
+import pandas as pd
 
 def main():
     print("")
@@ -23,13 +24,34 @@ if response.status_code == 200:
 else:
     print(f"Error: {response.status_code} - {response.text}")
 
-# format values
+# Format values
 tempCelsius = weatherData['current']['temp_c']
 tempFahrenheit = weatherData['current']['temp_f']
 
 # hold Alt + 0176 to get the degree symbol
 print(f"Celsius temperature in {cityName}: {tempCelsius:}°C")
 print(f"Fahrenheit temperature in {cityName}: {tempFahrenheit:}°F")
+
+# List of cities we want to collect the data of
+cities = ["London", "Birmingham", "Glasgow", "Liverpool", "Bristol", "Manchester", "Sheffield", "Leeds", "Edinburgh", "Leicester"]
+
+citiesList = []
+
+# For each city, we access the data using the Weather API
+for city in cities:
+    # Final Url:
+    finalUrl = baseURL + "key=" + apiKEY + "&q=" + city
+
+    response = requests.get(finalUrl)
+
+    if response.status_code == 200:
+        cityData = response.json()  # Call .json() method here
+        citiesList.append([cityData['location'], cityData['current']])
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+dataset = pd.DataFrame(citiesList)
+print(dataset.head())
 
 if __name__ == "__main__":
     main()
