44ddcfb89488be04d2c2b8ec97c0330ea642336e
TiffyPox <tiffanyjaynedavies@hotmail.co.uk>
2025-01-04 16:42:05 +0000

    update classes

--- src/test2.py
+++ src/test2.py
@@ -29,30 +29,5 @@ def main():
 
     print(f"\nNumbers you have already guessed: {already_guessed}")
 
-    print("Guess 4 numbers between 0-9 separated with a space:")
-
-    response = (input())
-
-    guesses = response.split()
-
-    for i in range(len(guesses)):
-        guesses[i] = int(guesses[i])
-
-    print(f"Your numbers are: {guesses}")
-
-    for i in range(len(guesses)):
-        already_guessed.append(guesses[i])
-
-        if guesses[i] == nums[i]:
-            print("Y", end=" ")  # Number and position is correct
-        elif guesses[i] in nums:
-            print("N", end=" ")  # Number is correct but wrong place
-        else:
-            print("X", end=" ")  # Number is incorrect
-
-    print(f"\nNumbers you have already guessed: {already_guessed}")
-
-    print("Guess 4 numbers between 0-9 separated with a space:")
-
 if __name__ == '__main__':
     main()
